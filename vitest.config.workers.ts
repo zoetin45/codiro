486a04cc5430032e091a5b6a6c0ddb4048230b54
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2025-10-07 09:51:21 +0900

    specify with config

--- vitest.config.workers.ts
+++ vitest.config.workers.ts
@@ -2,6 +2,7 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 
 export default defineWorkersConfig({
   test: {
+    include: ['worker/**/*.{test,spec}.{js,ts}'],
     poolOptions: {
       workers: {
         wrangler: { configPath: './wrangler.jsonc' },
